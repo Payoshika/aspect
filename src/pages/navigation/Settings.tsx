6f9c98bf8fc19a6d9ec127a886594ba6d572c675
大石航平 <oishikohei@ooishikouheis-MacBook-Pro.local>
2025-08-14 18:39:31 +0100

    responsive navigation containers added

---
+++ src/pages/navigation/Settings.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { HugeiconsIcon } from '@hugeicons/react';
+import { Location09Icon, UserMultiple03Icon, Invoice02Icon, SecurityCheckIcon, Notification01Icon, SquareUnlock02Icon, UserIcon } from '@hugeicons/core-free-icons';
+
+const Settings: React.FC = () => {
+  const settingsMenuItems = [
+    { name: 'Locations', icon: <HugeiconsIcon icon={Location09Icon} />},
+    { name: 'Users', icon: <HugeiconsIcon icon={UserMultiple03Icon} /> },
+    { name: 'Billing', icon: <HugeiconsIcon icon={Invoice02Icon} /> },
+    { name: 'Invoices', icon: <HugeiconsIcon icon={SecurityCheckIcon} /> },
+    { name: 'Legal', icon: <HugeiconsIcon icon={Location09Icon} /> },
+    { name: 'Notifications', icon: <HugeiconsIcon icon={Notification01Icon} /> },
+  ];
+
+  return (
+    <div className="mt-auto mb-10">
+      <hr className="hidden border-t border-gray-200 my-2 lg:block" />
+      <h3 className="px-4 py-2 text-xs font-semibold tracking-wider">
+        Settings
+      </h3>
+      <nav className="px-2">
+        {/* Show Profile as another option on small screens */}
+        <div className="block lg:hidden">
+            <a
+            href="#"
+            className="group flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 transition-colors duration-200"
+            >
+            <div className="flex items-center gap-2 lg:flex-row flex-row-reverse w-full">
+                <span className="text-lg">
+                <HugeiconsIcon icon={UserIcon} />
+                </span>
+                <span>Profile</span>
+            </div>
+            </a>
+        </div>
+        {settingsMenuItems.map((item) => (
+          <a
+            key={item.name}
+            href="#"
+            className="group flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 transition-colors duration-200"
+          >
+            <div className="flex items-center gap-2 lg:flex-row flex-row-reverse w-full">
+              <span className="text-lg">{item.icon}</span>
+              <span>{item.name}</span>
+            </div>
+          </a>
+        ))}
+      {/* Show Log-out as another option on small screens */}
+      <div className="block lg:hidden">
+        <a
+          href="#"
+          className="group flex items-center px-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 transition-colors duration-200"
+        >
+          <div className="flex items-center gap-2 lg:flex-row flex-row-reverse w-full">
+            <span className="text-lg">
+              <HugeiconsIcon icon={SquareUnlock02Icon} />
+            </span>
+            <span>Log-out</span>
+          </div>
+        </a>
+      </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Settings;
